70c3c6e40f640994a1f41ec4476c013cb7df1056
lov3b <lovebillenius@protonmail.com>
2021-11-21 19:16:58 +0100

    Check if the user asks non-accepted requst before getting webpage

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,34 @@ use reqwest;
 use std::env;
 
 fn main() {
-    let help = "Options for this program:\n-a or --all for all \n--win for Win64 \n--wow for WOW64 \n--webkit for AppleWebKit";
+    let help = "Options:
+        -a or --all for all 
+        --win for Win64 
+        --wow for WOW64 
+        --webkit for AppleWebKit
 
+        There must be one argument,and one only";
+    // Argument handeling
+    let args: Vec<String> = env::args().collect();
+    let param = &match args.get(1) {
+        Some(ok) => ok,
+        None => {
+            println!("{}", help);
+            std::process::exit(0);
+        }
+    };
+    let conf: Conf = match param.as_str() {
+        "-a" | "--all" => Conf::All,
+        "--win" => Conf::Win,
+        "--wow" => Conf::Wow,
+        "--webkit" => Conf::Webkit,
+        _ => {
+            println!("{}", &help);
+            std::process::exit(0);
+        }
+    };
+
+    // Get the page
     let resp = reqwest::blocking::get(
         "https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome",
     )
@@ -12,6 +38,7 @@ fn main() {
     .unwrap();
 
     let mut user_agents: Vec<String> = Vec::new();
+
     // Get useragent from html
     for i in resp.lines() {
         if i.contains("Windows NT 10.0") {
@@ -24,34 +51,27 @@ fn main() {
         }
     }
 
-    // Argument
-    let args: Vec<String> = env::args().collect();
-    let param = &match args.get(1) {
-        Some(ok) => ok,
-        None => {
-            println!("{}", help);
-            std::process::exit(0);
-        }
-    };
-
-    // Handle argument given
-    match param.as_str() {
-        "-a" | "--all" => {
+    // Do as the user wishes
+    match conf {
+        Conf::All => {
             for i in 0..user_agents.len() {
                 println!("{}", user_agents[i]);
             }
         }
-        "--win" => {
+        Conf::Win => {
             println!("{}", user_agents[0]);
         }
-        "--wow" => {
+        Conf::Wow => {
             println!("{}", user_agents[1]);
         }
-        "--webkit" => {
+        Conf::Webkit => {
             println!("{}", user_agents[2]);
         }
-        _ => {
-            println!("{}", &help);
-        }
     }
+}
+enum Conf {
+    All,
+    Win,
+    Wow,
+    Webkit,
 }
